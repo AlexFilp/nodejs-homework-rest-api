94250df8a36343c5a7447aa858f7768501791920
Oleksandr FIlippov <filya170895@gmail.com>
2023-05-11 20:21:45 +0200

    copied old branch

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 require("dotenv").config();
 const app = require("./app");
+const mongoose = require("mongoose");
 
+const { DB_HOST } = process.env;
 const { PORT } = process.env;
 
-app.listen(PORT, () => {
-  console.log(`Server running. Use our API on port: ${PORT}`);
-});
+console.log(process.env.DB_HOST);
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    console.log("Database connection successful");
+    app.listen(PORT, () => {
+      console.log(`Server running. Use our API on port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
