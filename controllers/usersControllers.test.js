0e21c3d198448390bef0aafcaf9b08838167b196
Oleksandr FIlippov <110115726+AlexFilp@users.noreply.github.com>
2023-05-28 09:32:38 +0200

    installed jest and supertest

---
+++ controllers/usersControllers.test.js
@@ -0,0 +1,34 @@
+// require("dotenv").config();
+// const mongoose = require("mongoose");
+// const app = require("../app");
+// const request = require("supertest");
+
+// const { DB_HOST, PORT = 3000 } = process.env;
+
+// const { login } = require("./usersControllers");
+// const { authenticate } = require("../middlewares");
+
+// app.post("/api/users/login", authenticate, login);
+
+// describe("test login controller", () => {
+//   beforeAll(() =>
+//     mongoose
+//       .connect(DB_HOST)
+//       .then(() => {
+//         console.log("Database connection successful");
+//         app.listen(PORT, () => {
+//           console.log(`Server running. Use our API on port: ${PORT}`);
+//         });
+//       })
+//       .catch((error) => {
+//         console.log(error.message);
+//         process.exit(1);
+//       })
+//   );
+//   afterAll(() => process.exit(1));
+
+//   test("login return status 200", async () => {
+//     const response = await request(app).post("/api/users/login");
+//     expect(response.status).toBe(200);
+//   });
+// });
